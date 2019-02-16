905afa451f16dc6fc3dfbdd0142fdc5f6cdff5eb
Leo Poulson <lp16860@my.bristol.ac.uk>
2019-02-16 16:26:48 +0000

    define epistemic models

--- Model.hs
+++ Model.hs
@@ -1,7 +1,9 @@
 module Model where
 
-data Agent = Ag Int deriving (Eq, Ord)
+newtype Agent = Ag Int deriving (Eq, Ord)
 
+-- We have an infinite amount of possible agents
+-- But the first five get names!
 a, b, c, d, e :: Agent
 a = Ag 0; b = Ag 1; c = Ag 2; d = Ag 3; e = Ag 4
 
@@ -10,3 +12,25 @@ instance Show Agent where
     show (Ag 2) = "Agent c"; show (Ag 3) = "Agent d"
     show (Ag 4) = "Agent e"
     show (Ag n) = "Agent " ++ show n
+
+
+type World = Int
+
+data Prop = Top | P Char | Not Prop | And Prop Prop 
+
+data PropK = Prop | K Agent Prop
+
+data EpistM = Mo 
+    [World]              -- Set of possible worlds
+    [Agent]              -- Set of agents in model
+    [(World, [Prop])]    -- Valuation function; \pi : World -> Set of props.
+    [(Agent, [[World]])] -- Epistemic relation between worlds
+    [World]              -- Set of initial worlds. 
+
+
+
+
+
+
+
+
