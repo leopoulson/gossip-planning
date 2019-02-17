4ada2b3f682c439d695a9790be36b077acd0313f
Leo Poulson <lp16860@my.bristol.ac.uk>
2019-02-17 13:32:04 +0000

    implement satisfies

--- Model.hs
+++ Model.hs
@@ -13,35 +13,58 @@ instance Show Agent where
     show (Ag 4) = "Agent e"
     show (Ag n) = "Agent " ++ show n
 
-
 type World = Int
 type Rel = [[World]]
 
-data Prop = Top | P Char | Not Prop | And Prop Prop 
-
-data PropK = Prop | K Agent Prop
+-- TODO: Find a better way to do this
+data Prop = Top | P Int | Not Prop | And Prop Prop | K Agent Prop deriving (Eq, Show)
+-- data PropK = Pr Prop | K Agent Prop
 
 data EpistM = Mo 
-    [World]              -- Set of possible worlds
-    [Agent]              -- Set of agents in model
-    [(World, [Prop])]    -- Valuation function; \pi : World -> Set of props.
-    [(Agent, Rel)] -- Epistemic relation between worlds
-    [World]              -- Set of initial worlds. 
+    [World]           -- Set of possible worlds
+    [Agent]           -- Set of agents in model
+    [(Int, [Prop])]  -- Valuation function; \pi : World -> Set of props.
+    [(Agent, Rel)]    -- Epistemic relation between worlds
+    [World]           -- Set of pointed worlds.
+
+type PointedEM = (EpistM, World)  -- This is a pointed model. 
 
 example :: EpistM
 example = Mo 
     [0 .. 3]
     [a, b, c]
-    []
+    [(0, [P 2])]
     [(a, [[0], [1], [2], [3]]), (b, [[0], [1], [2], [3]]), (c, [[0 .. 3]])]
     [1]
 
-rel :: Agent -> EpistM -> Rel
-rel ag (Mo _ _ _ rels _) = table2fn rels ag
+-- This lets us access the relations for a given agent
+rel :: EpistM -> Agent -> Rel
+rel (Mo _ _ _ rels _) ag = table2fn rels ag
+
+-- This gets the worlds related to world w
+-- TODO: Perhaps change from head $
+relatedWorlds :: Rel -> World -> [World]
+relatedWorlds r w = concat $ filter (elem w) r
+
+val :: EpistM -> World -> [Prop]
+val (Mo _ _ vals _ _) wo = table2fn vals wo
 
 -- TODO: Consider changing default value to error?
-table2fn :: [(Agent, Rel)] -> Agent -> Rel
+table2fn :: Eq a => [(a, [b])] -> a -> [b]
 table2fn t ag = maybe [] id (lookup ag t)
+
+-- Give a semantics!
+satisfies :: PointedEM -> Prop -> Bool
+satisfies _ Top = True
+satisfies (m, w) (P n) = P n `elem` val m w
+satisfies (m, w) (Not p) = not $ satisfies (m, w) p
+satisfies (m, w) (And p q) = and (satisfies (m, w) p, satisfies (m, w) q)
+satisfies (m, w) (K ag p) = all (\v -> satisfies (m, v) p) rw 
+  where 
+    r :: Rel
+    r = rel m ag
+    rw :: [World]
+    rw = relatedWorlds r w
 
 
 
