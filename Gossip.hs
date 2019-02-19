cfb7ea3eb8b2a21c8c734520ad624622e810e638
Leo Poulson <lp16860@my.bristol.ac.uk>
2019-02-19 13:52:10 +0000

    back to using map & set, support possible calls

--- Gossip.hs
+++ Gossip.hs
@@ -3,17 +3,32 @@ module Gossip where
 import Model
 import qualified Data.Map as M
 import qualified Data.Set as S
+import Data.Maybe
 
---                                Number   Secret
-newtype GGraph = Gossip ([Agent], KnowMap, KnowMap)
+--                              Number  Secret
+data GGraph = Gossip [Agent] KnowMap KnowMap
     deriving (Show, Eq)
+-- data GGraph = Gossip [Agent] [Prop]
+--     deriving (Show, Eq)
 
 type KnowMap = M.Map Agent (S.Set Agent)
 
 type Call = (Agent, Agent)
 
-gExample :: GGraph
-gExample = Gossip ([a, b, c], M.fromList [(a, S.singleton b), (b, S.singleton c)], M.empty)
+-- gExample :: GGraph
+-- gExample = Gossip [a, b, c] [N a b, N b c]
 
-call :: GGraph -> Call -> GGraph
-call g c = 
+gExample :: GGraph
+gExample = Gossip [a, b, c] (M.fromList [(a, S.singleton b), (b, S.singleton c)]) M.empty
+
+possible :: Call -> GGraph -> Bool
+possible (i, j) (Gossip _ n _) = S.member j (fromMaybe S.empty (M.lookup i n) )
+
+-- call :: GGraph -> Call -> GGraph
+-- call g c 
+--     | possible c g  = 
+--     | otherwise = error $ "impossible call " ++ show c
+
+
+
+
