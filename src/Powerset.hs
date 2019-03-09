8489e767cc039852cb14576e9de445fbc3dc07be
Leo Poulson <lp16860@my.bristol.ac.uk>
2019-03-09 09:59:17 +0000

    Add in tests for powerset automata

--- src/Powerset.hs
+++ src/Powerset.hs
@@ -5,12 +5,14 @@ import FSM
 import FST
 import ME
 
+import Data.List (nub)
+
 -- data PState = PState { 
 --     state :: QState,  
 --     possStates :: [QState]
 -- }
 
-data PState = PState QState [QState]
+data PState = PState QState [QState] deriving (Eq, Show)
 
 buildPSA :: FSM Character QState -> FST Character QState -> FSM Character PState
 buildPSA fsm fst = FSM alphabet' states' transition' initial' accepting' where
@@ -22,7 +24,7 @@ buildPSA fsm fst = FSM alphabet' states' transition' initial' accepting' where
                  PState (FSM.transition fsm (state, ch))  -- the next "current" state
                         (getPossStates ch possStates)     -- the set of possible states we can be in
     getPossStates :: Character -> [QState] -> [QState]
-    getPossStates ch = concatMap (\ st -> map snd $ bitransition fst (st, ch))
+    getPossStates ch = nub . concatMap (\ st -> map snd $ bitransition fst (st, ch))
 
 psaFromScratch :: Agent -> EpistM -> EventModel -> FSM Character PState
 psaFromScratch ag ep ev = buildPSA dAuto (buildComposedSS ag ep ev dAuto)
--- src/Tests/PSTests.hs
+++ src/Tests/PSTests.hs
@@ -7,9 +7,43 @@ import SSFST
 import ME 
 import Powerset
 
--- psTest1 :: Test
--- psTest1 = "Test that transition for non-related states is correct"
---        ~:
+import Tests.Tests
+import Test.HUnit hiding (State)
+
+psTests :: Test
+psTests = TestList [psTest1, psTest2, psTest3]
+
+dopsTests :: IO Counts
+dopsTests = runTestTT psTests
+
+psTest1 :: Test
+psTest1 = "Test that transition for non-related states is correct" 
+       ~: PState (Q [N a b, N b a, S a b, S b a]) [Q [N a b, N b a, S a b, S b a]] ~=? t1
+
+psTest2 :: Test 
+psTest2 = "Test that transition for related states is correct"
+       ~: PState (Q [N a b, N b a, S a b, S b a]) [Q [N a b, N b a, S a b, S b a], Q [N b a]] ~=? t2
+
+psTest3 :: Test
+psTest3 = "There should be no duplicates for related states"
+       ~: PState (Q [N a b, N b a, S a b, S b a]) [Q [N a b, N b a, S a b, S b a]] ~=? t5
+
+t1 = powersetTrans (PState (Q [N a b]) [Q [N a b]], Right (Call a b))
+t2 = powersetTrans (PState (Q [N b a]) [Q [N b a]], Right (Call b a))
+t2' = powersetTrans (PState (Q [N b a]) [Q [N b a]], Right (Call a a))
+
+-- It's becoming time to consider what to do for a non-permitted call
+-- It seems to make the most sense to just return the empty list, thus making a 
+-- "non-transition" in the automata. 
+-- However, we need to consider what the implications of this will be elsewhere. 
+-- Will this be problematic for other cases
+t3 = powersetTrans (PState (Q [N b a]) [Q [N b a], Q [N b a]], Right (Call b a))
+t4 = powersetTrans (PState (Q [N b a]) [Q [N b a], Q [N b a], Q [N b a, S b c]], Right (Call b a))
+t5 = powersetTrans (PState (Q [N b a, S b a, N a b, S a b]) [Q [N b a, S b a, N a b, S a b]], Right (Call b a))
+
+
+-- psTest3 :: Test 
+-- psTest3 = "Test that result is identical for indistinguishable calls"
 
 powersetTrans :: Transition PState Character
 powersetTrans = transition powerset
@@ -20,7 +54,7 @@ powerset = psaFromScratch a model eventModel
 model :: EpistM
 model = Mo
     [State (0, []), State (1, []), State (2, [])]
-    [a, b]
+    [a, b, c]
     [(State (0, []), [P (N a b)]), (State (1, []), [P (N a b)]), (State (2, []), [P (N b a)])]
     [(a, [[State (0, [])], [State (1, []), State (2, [])]]), (b, [[State (0, [])], [State (1, []), State (2, [])]])]
     [State (0, [])]
