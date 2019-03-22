e2f297116b4af96cb5181d6dd4335a71b6b8e80a
Leo Poulson <leopoulson@gmail.com>
2019-03-22 11:51:28 +0000

    Add notes, some profiles

--- src/Main.hs
+++ src/Main.hs
@@ -10,7 +10,7 @@ import Powerset
 import RS
 
 main :: IO ()
-main = putStrLn $ show $ extractCalls $ doBFS saFour
+main = putStrLn $ show $ extractCalls $ doBFS saFive
 
 threeModel :: EpistM
 threeModel = Mo 
@@ -41,3 +41,18 @@ fourEvModel = standardEventModel [a, b, c, d] anyCall postUpdate
 saFour :: FSM Character (PState QState)
 saFour = createSolvingAutomata (K a $ allExpertsAg $ [a, b, c, d]) fourModel fourEvModel
 
+fiveEvModel :: EventModel
+fiveEvModel = standardEventModel [a, b, c, d, e] anyCall postUpdate
+
+fiveModel :: EpistM
+fiveModel = Mo
+    [State (0, [])]
+    [a, b, c, d, e]
+    [(State (0, []), [P (N a b), P (N b c), P (N c d), P (N e d)])]
+    [(a, [[State (0, [])]]), (b, [[State (0, [])]]), (c, [[State (0, [])]])]
+    [State (0, [])]
+
+saFive :: FSM Character (PState QState)
+saFive = createSolvingAutomata (K a $ allExpertsAg [a, b, c, d, e]) fiveModel fiveEvModel
+  
+                       
