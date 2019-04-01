4ccac84f6b22ff1e082759d53b7d9364633f60b8
Leo Poulson <lp16860@my.bristol.ac.uk>
2019-04-01 17:14:10 +0100

    Trying to fix problems, mainly not getting anywhere

--- src/BFSM.hs
+++ src/BFSM.hs
@@ -10,28 +10,47 @@ import Data.Maybe (mapMaybe)
 import Data.List (foldl')
 import Data.Set (Set, empty, insert, notMember)
 
+import Debug.Trace
 
 -- can probably use a lens here !!!!!! lol
 data BNode a ch = BNode {
     node   :: !a,       -- The actual node that we're looking at
     parent :: !(Maybe (BNode a ch, ch))  -- We might not have a predecessor yet
-} deriving (Eq, Show)
+} deriving (Eq)
 
-bNode :: a -> BNode a ch
+instance (Show a, Show ch) => Show (BNode a ch) where
+  show (BNode a ch) = show a ++ " Parent " ++ show ch
+
+bNode :: Show a => a -> BNode a ch
 bNode a = BNode a Nothing
 
 bNodePred :: a -> BNode a ch -> ch -> BNode a ch
 bNodePred n p ch = BNode n (Just (p, ch))
 
-doBFS :: Ord a => FSM ch a -> Maybe [(a, Maybe ch)]
+doBFS :: (Ord a, Show a, Show ch) => FSM ch a -> Maybe [(a, Maybe ch)]
 doBFS fsm = bfs fsm (map bNode $ initial fsm) empty
 
-bfs :: Ord a => FSM ch a -> [BNode a ch] -> Set a -> Maybe [(a, Maybe ch)]
-bfs fsm queue seen = case queue of
+doBFS' :: (Ord a, Show a, Show ch) => FSM ch a -> Maybe [(a, Maybe ch)]
+doBFS' fsm = bfs' fsm (Seq.fromList $ map bNode $ initial fsm) empty
+
+bfs :: (Ord a, Show a, Show ch) => FSM ch a -> [BNode a ch] -> Set a -> Maybe [(a, Maybe ch)]
+bfs fsm queue seen =
+  --trace ("Queue: " ++ show (queue) ++ "\nSeen: " ++ show (seen) ++ "\n\n") $
+  case queue of
     []     -> Nothing    -- If the queue is empty, we stop and that is that
-    (q:qs) -> case accepting fsm $ node q of
-        True  -> Just $ rebuildPath q                                     -- Here construct the path 
-        False -> bfs fsm (updateQueue fsm q qs seen) (insert (node q) seen)   -- Here we want to recurse
+    (q:qs) ->  case accepting fsm $ node q of
+            True  -> Just $ rebuildPath q                                     -- Here construct the path
+            False -> bfs fsm (updateQueue fsm q qs seen) (insert (node q) seen)   -- Here we want to recurse
+
+
+
+
+    {-  if (node q) `notMember` seen then
+          --trace ("Node: " ++ show (node q) ++ "\n") $
+          case accepting fsm $ node q of
+            True  -> Just $ rebuildPath q                                     -- Here construct the path
+            False -> bfs fsm (updateQueue fsm q qs seen) (insert (node q) seen)   -- Here we want to recurse
+       else  bfs fsm qs seen--}
 
 bfs' :: Ord a => FSM ch a -> Seq.Seq (BNode a ch) -> Set a -> Maybe [(a, Maybe ch)]
 bfs' fsm queue seen = case Seq.viewl queue of
@@ -40,16 +59,6 @@ bfs' fsm queue seen = case Seq.viewl queue of
         True  -> Just $ rebuildPath q                                     -- Here construct the path 
         False -> bfs' fsm (updateQueue' fsm q qs seen) (insert (node q) seen)   -- Here we want to recurse
 
-
---rebuildPath :: BNode a ch -> [(a, Maybe ch)]
---rebuildPath (BNode a (Just (bn, ch))) = (a, Just ch) : rebuildPath bn
---rebuildPath (BNode a Nothing)   = [(a, Nothing)]
-
-rebuildPath :: BNode a ch -> [(a, Maybe ch)]
-rebuildPath = go []
-  where
-    go acc (BNode x Nothing)         = (x, Nothing):acc
-    go acc (BNode x (Just (bn, ch))) = go ((x, Just ch):acc) bn
 
 -- this takes an fsm, a state, queue and seen & returns new queue
 updateQueue :: Ord a => FSM ch a -> BNode a ch -> [BNode a ch] -> Set a -> [BNode a ch]
@@ -104,6 +113,12 @@ enqueueOne seen item queue
 
 extractCalls :: Maybe [(a, Maybe ch)] -> Maybe [ch]
 extractCalls list = mapMaybe snd <$> list
+
+rebuildPath :: BNode a ch -> [(a, Maybe ch)]
+rebuildPath = go []
+  where
+    go acc (BNode x Nothing)         = (x, Nothing):acc
+    go acc (BNode x (Just (bn, ch))) = go ((x, Just ch):acc) bn
 
 
 
